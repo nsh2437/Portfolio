6ddab0e4ed07ea1c2bfceb68bd7fe6f0930d3e12
nsh2437 <119505410+nsh2437@users.noreply.github.com>
2022-12-13 00:52:32 +0900

    Delete DataBase/관련자료 directory

--- DataBase/관련자료/AddInformation.cs
+++
@@ -1,8 +0,0 @@
-public void AddInformation(string name, long score)
-{
-    Rank user = new Rank(name, score);
-    string json = JsonUtility.ToJson(user);
-    string key = _reference.Push().Key;
-    
-    _reference.Child(key).SetRawJsonValueAsync(json);
-}
